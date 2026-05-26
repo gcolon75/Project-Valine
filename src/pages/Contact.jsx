dbecde935c96b1fb13ae3870bc30c76f3dfa1f77
Finn383 <bmsullivan4@wisc.edu>
2026-05-26 16:43:29 -0500

    Fix Contact.jsx import to use named apiClient export

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import apiClient from '../services/api';
+import { apiClient } from '../services/api';
 
 /**
  * Contact Us Page
